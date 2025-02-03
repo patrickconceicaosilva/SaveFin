ba60d3e3d222e3380212ea53b4210667a95cec56
Patrick da Conceição Silva <151814907+patrickconceicaosilva@users.noreply.github.com>
2025-02-03 02:18:44 -0300

    Implement language selection (Portuguese and English) in OptionMenu

--- main.py
+++ main.py
@@ -35,6 +35,18 @@ def getInput():
     registro_produto.focus_set()
 
     print(f"Produto salvo em {arquivo_JSON}")
+
+def mudarIdioma():
+    idioma = idiomaVar.get()
+
+    if idioma == "Português":
+        label_produto.config(text="Produto")
+        label_valor.config(text="Valor")
+        botao_submit.config(text="Enviar")
+    else:
+        label_produto.config(text="Product")
+        label_valor.config(text="Price")
+        botao_submit.config(text="Submit")
     
 #Janela Tkinter
 
@@ -44,20 +56,26 @@ window.geometry("400x500")
 window.resizable(width = False, height= False)
 window.title("SaveFin")
 
+idiomaVar = tkinter.StringVar()
+idiomaVar.set("English")
+
 frame = tkinter.Frame(window)
 frame.pack(expand=True)
 
-label_produto = tkinter.Label(frame, text="Produto", font=("Arial", 15))
+label_produto = tkinter.Label(frame, text="Product", font=("Arial", 15))
 label_produto.pack(pady=20)
 registro_produto = tkinter.Entry(frame, font=("Arial", 15))
 registro_produto.pack()
 
-label_valor = tkinter.Label(frame, text="Valor", font=("Arial", 15))
+label_valor = tkinter.Label(frame, text="Price", font=("Arial", 15))
 label_valor.pack(pady=20)
 registro_valor = tkinter.Entry(frame, font=("Arial", 15))
 registro_valor.pack()
 
-botao_registro = tkinter.Button(frame, text="Registrar", command=getInput, font=("Arial", 15))
-botao_registro.pack(pady=20)
+botao_submit = tkinter.Button(frame, text="Submit", command=getInput, font=("Arial", 15))
+botao_submit.pack(pady=20)
+
+dropDownIdioma = tkinter.OptionMenu(window, idiomaVar, "English", "Português", command=lambda _: mudarIdioma())
+dropDownIdioma.pack()
 
 window.mainloop()
